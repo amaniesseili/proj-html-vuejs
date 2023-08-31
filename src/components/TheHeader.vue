20e38383d92ff15a6c5be4efe5db22b1dd50c5b6
amani esseili <esseiliamani@hotmail.com>
2023-08-31 23:08:31 +0200

    TheHeader-done

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -10,13 +10,31 @@ export default{
 
 <template>
   <header>
-    <div class="logo">
-      <img src="" alt="">
+    
+    <div class="container">
+      <div class="header-info">
+        <div class="d-flex">
+          <p><small>Download App</small></p>
+          <p class="ms-5"><small>Advertise with us</small></p>
+        </div>
+        <div class="social-icons">
+          <i class="fa-brands fa-facebook-f"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-twitter"></i>
+          <i class="fa-brands fa-youtube"></i>
+        </div>
+      </div>
     </div>
 
+
+
+    <div class="logo">
+      <img class="logo-img" src="../assets/img/avada-food-logo-2x.png" alt="avad-food-logo">
+    </div>
+
+    <!-- 3-VOCI MENU: creo il ciclo for  -->
     <nav>
       <div class="navigation">
-        <!-- 3-VOCI MENU: creo il ciclo for  -->
         <div class="nav-menu" v-for="item in navigationItems" :key="item.id">
           {{item.title}}
         </div>
@@ -41,8 +59,30 @@ export default{
   justify-content: center;
   align-items: center;
   gap: 4rem;
-  
+  margin-bottom: 1rem;
+  padding-bottom: 0.5rem;
+  margin-bottom: 0.5rem;
+
 }
+
+.logo{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+
+}
+
+.logo-img{
+  width: 150px;
+}
+
+.header-info{
+  display: flex;
+  justify-content: space-between;
+}
+
 
 
 </style>
