9f2906e4795610a05d30f860df841e44079bbf6c
amani esseili <esseiliamani@hotmail.com>
2023-08-31 19:35:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script >
+import TheHeader from './components/TheHeader.vue';
+import TheMain from './components/TheMain.vue';
+import TheFooter from './components/TheFooter.vue';
+
+
+
+export default {
+    components: { 
+      TheFooter,
+      TheHeader,
+      TheMain
+    }
+}
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+
+  <TheMain></TheMain>
+
+  <TheFooter></TheFooter>
+  
+</template>
+
+<style lang="scss">
+
+</style>
