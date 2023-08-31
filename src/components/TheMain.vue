12c7454f575915f8b2b933e43d36e619f42d37c4
amani esseili <esseiliamani@hotmail.com>
2023-09-01 00:25:54 +0200

    jumbo content in position absolute

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -11,23 +11,12 @@ export default{
       <div class="jumbo-img"></div>
 
       <div class="jumbo-content">
-        <div class="row">
-          <div class="col-5"></div>
-          <div class="col-2">
-            <button class="jumbo-btn">TODAY'S PICK</button>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-5"></div>
-          <div class="col-3">
-            <div class="jumbo-text text-center bg-light pt-5 pb-4 rounded-3">
-              <h2>Food Corner: Top Japanese <br>Resaurant for Sushi</h2>
-              <small>March 25.2019</small>
-            </div>
-
-
-
-          </div>
+        
+        <button class="jumbo-btn">TODAY'S PICK</button>
+        
+        <div class="jumbo-text text-center bg-light pt-4 pb-3">
+          <h2>Food Corner: Top Japanese <br>Resaurant for Sushi</h2>
+          <small>March 25.2019</small>
         </div>
 
       </div>
@@ -50,12 +39,21 @@ export default{
   background-repeat: no-repeat;
   background-position: center;
   width: 100%;
+  position: relative;
 }
 .jumbo-btn{
   background-color: $primary-color;
   color: white;
   border-radius: 10px;
   padding: 0.2rem 1rem;
+}
+.jumbo-content{
+  position: absolute;
+  top:50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+
 }
 
 </style>
