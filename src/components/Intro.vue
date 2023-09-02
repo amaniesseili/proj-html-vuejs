a1e97ec91e9635a662f472c5e7c1d75f20e51e6f
amani esseili <esseiliamani@hotmail.com>
2023-09-02 23:47:26 +0200

    intro slide -done

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -18,7 +18,42 @@ export default{
   <div>
     <div v-if="showIntro" class="intro">
 
-      <p>benvenuti al mio progetto </p>
+      <div class="header">
+        
+      </div>
+
+      <section class="cards">
+        <div class="container ">
+  
+          <div class="card-container d-flex justify-content-between h-100  ">
+  
+            <div class="card me-5" style="width: 20rem;">
+                <img src="src/assets/img/floria.jpg" class="card-img-top" alt="...">
+              <div class="card-body ">
+                <h5 class="text-center">MAESTRO: <br> Leica Florian Robert</h5>
+              </div>
+            </div>
+            
+            <div class="card me-5" style="width: 20rem;">
+              <img src="src/assets/img/ama.jpg" class="card-img-top" alt="...">
+              <div class="card-body ">
+                <h5 class="text-center">STUDENTE: <br> Amani Esseili</h5>
+              </div>
+            </div>
+
+          <div class="card " style="width: 20rem;">
+              <img src="src/assets/img/boolean-logo.png" class="card-img-top" alt="...">
+            <div class="card-body ">
+              <h5 class="text-center">TUTOR: <br> Alessio Crea & Michele Spiller</h5>
+            </div>
+        </div>
+          </div>
+        </div>
+
+        <h1>FOOD AVADA : WordPress Theme!</h1>
+      </section> 
+  
+
     </div>
   </div>
 
@@ -27,12 +62,41 @@ export default{
 <style lang="scss" scoped>
 @use "../scss/partials/variables" as *;
 
-.intro{
+
+
+.header{
+  min-height: 400px;
+  background-image: url(../assets/img/1-jumbo.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0.5;
   width: 100%;
-  height: 100wh;
+  border-left: 70px solid orangered;
+  border-top: 70px solid orangered ;
+  margin-bottom: 500px;
+
 }
 
-p{
-  font-size: 100px;
+.cards{
+  position: absolute;
+  top: 350px;
+  left: 50%;
+  transform: translatex(-50%);
+  
 }
+
+
+h1{
+  margin-top: 30px;
+  font-size: 30px;
+  font-weight: bold;
+  text-align: center;
+  vertical-align: middle;
+  color: black;
+  margin-bottom: 50px;
+
+
+}
+
 </style>
