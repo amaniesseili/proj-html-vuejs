4aa19fc655aa6af0dd951d7e867ff486740864df
amani esseili <esseiliamani@hotmail.com>
2023-09-04 15:14:32 +0200

    slide analisi modificata

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -12,7 +12,7 @@ export default{
     setTimeout(() => {
         this.showIntro = false;      // 1- nascondo la prima slide dopo 5s 
         this.indexSlide = true;    // 2- mostro la seconda slide 
-    }, 2500);
+    }, 3000);
 
     setTimeout(()=>{
       this.indexSlide= false;     //3- nascondo la seconda slide dopo altre 10s
@@ -22,11 +22,11 @@ export default{
     setTimeout(()=>{
       this.analisiSlide= false;     //5- nascondo la terza slide dopo altre 90s
       this.structureSlide= true;     //6-mostro la quarta slide 
-    }, 90000);
+    }, 8000);
 
     setTimeout(()=>{
       this.structureSlide= false      //7- structureSlide si scompare dopo 100s
-    }, 500000);
+    }, 20000);
   },
 
 };
@@ -105,43 +105,65 @@ export default{
       <p>Il layout del sito è suddiviso in tre sezioni princhipali: <b>Header</b><b> Main</b> e <b>Footer</b>.</p>
       
       <div class="header-analysis">
-        <h4>Header</h4>
-        <ul>
-          <li> La sua struttura e semplice presenta il <b>Logo</b> e il <b>Menu di Navigazione</b></li>
-        </ul>
-      </div>
+        <div class="row">
+          <h4>Header & Footer</h4>
+          <div class="col-11 p-color">
+            <ul>
+              <li>Struttura e semplice presenta il <b>Logo</b> e il <b>Menu di Navigazione</b> e<b> contatto</b> </li>
+            </ul>
+          </div>
+        </div>
+
+
       <div class="main-analysis">
-        <h4>Main</h4>
-        <p>Il Main contiene <b> 6 sezioni</b> ognuna con un contenuto ben definito e layout molto simile </p>
-        <ul>
-          <li><b> Containers di Bootstrap</b></li>
-          <p>i container sono <b> uguali</b> in tutti le sezioni, con <b>spaziatura specifica</b> sia sopra che sotto il containerper garantire una layout equilibrato. </p>
-        </ul>
-        <ul>
-          <li><b>Contenuto Centrato </b></li>
-          <p>Tanti sezioni hanno il <b>Test Centrato </b>.</p>
-        </ul>
-        <ul>
-          <li><b>Card Bootstrap</b></li>
-          <p>Le Card di Bootstrap sono <b>identici</b> in ogni sezione, ognuna con immagine e corpo informativo.</p>
-        </ul>
-        <ul>
-          <li><b>overlay sull'immagine</b></li>
-          <p>Quasi tutti gli immagine hanno un overlay con un test centralizato.</p>
-        </ul>
-        <ul>
-          <li><b>Scelta dei Colori</b></li>
-          <p>I colori utilisati riflettono la tema culinario del sito e trasmettono specifici significati:
+        <div class="row">
+          <h4>Main</h4>
+          <p>Il Main contiene <b> 6 sezioni</b> ognuna con un contenuto ben definito e layout molto simile </p>
+
+          <div class="col-5 container-bootstrap">
+            <ul>
+              <li><b> Containers di Bootstrap</b></li>
+              <p>i container sono <b> uguali</b> in tutti le sezioni, con <b>spaziatura specifica</b> sia sopra che sotto il containerper garantire una layout equilibrato. </p>
+            </ul>
+          </div>
+          <div class="col-5 Contenuto-Centrato">
+            <ul>
+              <li ><b>Contenuto Centrato </b></li>
+              <p>Tanti sezioni hanno il <b>Test Centrato </b>.</p>
+            </ul>
+          </div>
+
+        </div>
+        <div class="row">
+          <div class="col-5 Card-Bootstrap">
+            <ul>
+              <li><b>Card Bootstrap</b></li>
+              <p>Le Card di Bootstrap sono <b>identici</b> in ogni sezione, ognuna con immagine e corpo informativo.</p>
+            </ul>
+          </div>
+          <div class="col-5 overlay-analysis">
+            <ul>
+              <li><b>overlay sull'immagine</b></li>
+              <p>Quasi tutti gli immagine hanno un overlay con un test centralizato.</p>
+            </ul>
+          </div>
+        </div>
+        <div class="row">
+          <h5>Scelta dei Colori</h5>
+          <p>I colori utilisati riflettono la tema culinario del sito e trasmettono specifici significati:</p>
+          <div class="col-5 me-4  p-color">
             <ul>
               <li><b>Sfondo Pulito e Neutro</b></li>
               <p>il colore dello sfondo e chiaro e neutro per creare un ambiente <b>Accogliente</b> e <b>Pulito</b> . </p>
             </ul>
+          </div>
+          <div class="col-5 ms-4 p-color">
             <ul>
               <li><b>Colore Arancione</b></li>
-              <p>L'uso del colore arancione rapresenta energia e movimento, trasmetendo una senzazione di accoglienza e creativitàai visitatori. </p>
+              <p>l'arancione rapresenta l'energia, trasmetendo una senzazione di accoglienza ai visitatori. </p>
             </ul>
-          </p>
-        </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -150,8 +172,6 @@ export default{
 <div v-if="structureSlide" class="structure ">
   <div class="content-container slide-layout">
     <div class="container">
-
-
       <div class="row">
         <div class="col-6" >
           <div class="titolo-slide">
@@ -207,12 +227,8 @@ export default{
       </div>
 
     </div>
-
-  
-
-
   </div>
-
+ </div>
 
 </div>
 
@@ -319,4 +335,37 @@ h1{
   background-color: #8ECECD;
   
 }
+
+.container-bootstrap{
+  border: 5px solid #3379B4;
+  margin-right: 3rem;
+  margin-bottom: 1rem;
+  margin-left: 2rem;
+  padding: 0.2rem;
+}
+.Contenuto-Centrato{
+  background-color: #87E5C0; 
+  margin-bottom: 1rem;
+  padding: 0.2rem;
+
+}
+.Card-Bootstrap{
+  background-color: #DAAFF7;
+  margin-right: 3rem;
+  margin-bottom: 1rem;
+  margin-left: 2rem;
+  padding: 0.2rem;
+}
+.overlay-analysis{
+  background-color: #FA779F;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  padding: 0.3rem;
+}
+.p-color{
+  background-color: $primary-color;
+  margin-left: 2rem;
+
+}
+
 </style>
